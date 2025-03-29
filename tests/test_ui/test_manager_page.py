82b0423ae57b1c5817403631c57f665d0e6a985d
Виталий Личинин <lichinin.v@yandex.ru>
2025-03-29 21:20:07 +0300

    Объединил тесты в класс TestManagerPage

--- tests/test_ui/test_manager_page.py
+++ tests/test_ui/test_manager_page.py
@@ -6,39 +6,48 @@ from helpers.data_helper import DataHelper
 
 @allure.epic('SimbirSoft SDET practicum')
 @allure.suite('UI tests')
-@allure.title('Тест добавления Customer')
-@allure.description('Проверка создания нового Customer и сообщения об успешном создании.')
-def test_customer_add(manager_page, customer_data):
-    manager_page.click_add_customer_menu_button()
-    manager_page.fill_first_name_field(customer_data['first_name'])
-    manager_page.fill_last_name_field(customer_data['last_name'])
-    manager_page.fill_post_code_field(customer_data['code'])
-    manager_page.click_add_customer_submit_button()
-    with allure.step('Проверка сообщения о создании пользователя'):
-        assert manager_page.get_alert_message(), Constants.EXPECTED_CUSTOMER_ADD_MESSAGE
+class TestManagerPage:
+    @allure.title('Тест добавления Customer')
+    @allure.description(
+        'Проверка создания нового Customer и сообщения об успешном создании.'
+    )
+    def test_customer_add(self, manager_page, customer_data):
+        manager_page.click_add_customer_menu_button()
+        manager_page.fill_first_name_field(customer_data['first_name'])
+        manager_page.fill_last_name_field(customer_data['last_name'])
+        manager_page.fill_post_code_field(customer_data['code'])
+        manager_page.click_add_customer_submit_button()
+        with allure.step('Проверка сообщения о создании пользователя'):
+            assert (
+                manager_page.get_alert_message()
+                == Constants.EXPECTED_CUSTOMER_ADD_MESSAGE
+            )
 
+    @allure.title('Тест сортировки Customers по First Name')
+    @allure.description(
+        'Сортировка Customers по First_Name и проверка корректности сортировки'
+    )
+    def test_customers_sort_by_name(self, manager_page):
+        manager_page.click_customers_menu_button()
+        manager_page.click_twice_first_name_column()
+        manager_page.get_customers_name()
+        with allure.step('Проверка сортировки списка Customer'):
+            assert (
+                manager_page.actual_customers_name
+                == manager_page.sorted_customer_name
+            )
 
-@allure.epic('SimbirSoft SDET practicum')
-@allure.suite('UI tests')
-@allure.title('Тест сортировкт Customers по First Name')
-@allure.description('Сортировка Customers по First_Name и проверка корректности сортировки')
-def test_customers_sort_by_name(manager_page):
-    manager_page.click_customers_menu_button()
-    manager_page.click_twice_first_name_column()
-    manager_page.get_customers_name()
-    with allure.step('Проверка сортировки свиска Customer'):
-        assert manager_page.actual_customers_name, manager_page.sorted_customer_name
-
-
-@allure.epic('SimbirSoft SDET practicum')
-@allure.suite('UI tests')
-@allure.title('Тест удаления Customer')
-@allure.description('Удаление Customer и проверка его отсутствия в списке Customers.')
-def test_customer_delete(manager_page):
-    manager_page.click_customers_menu_button()
-    manager_page.get_customers_name()
-    customer_to_delete = DataHelper.choice_name_to_delete(manager_page.actual_customers_name)
-    manager_page.click_delete_button(customer_to_delete)
-    manager_page.get_customers_name()
-    with allure.step('Проверка отсутствия удаленного Customer в списке'):
-        assert customer_to_delete not in manager_page.actual_customers_name
+    @allure.title('Тест удаления Customer')
+    @allure.description(
+        'Удаление Customer и проверка его отсутствия в списке Customers.'
+    )
+    def test_customer_delete(self, manager_page):
+        manager_page.click_customers_menu_button()
+        manager_page.get_customers_name()
+        customer_to_delete = DataHelper.choice_name_to_delete(
+            manager_page.actual_customers_name
+        )
+        manager_page.click_delete_button(customer_to_delete)
+        manager_page.get_customers_name()
+        with allure.step('Проверка отсутствия удаленного Customer в списке'):
+            assert customer_to_delete not in manager_page.actual_customers_name
